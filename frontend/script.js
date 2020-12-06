350d4b22c9c3a7c538faaae925d7c8dc68ea459f
Cemil Okay <cemilokay@gmail.com>
2020-12-06 23:47:29 +0000

    Fix comment line

--- frontend/script.js
+++ frontend/script.js
@@ -1,28 +1,41 @@
 // add item event listener
-document.getElementById('add_item').addEventListener('click', addItem)
+document.getElementById('add_item').addEventListener('click', saveTodoItem)
 
-// add item functiton
-function addItem() {
+// save todoitem to database
+function saveTodoItem() {
   if(document.getElementById('input_box').value.trim()){
-    const listItem = document.createElement('li');
-    listItem.className = 'list-item';
-    listItem.innerHTML = `
-      <span class="todo-item">${document.getElementById('input_box').value}</span>
-      <span class="edit-item">(edit)</span>
-      <span class="remove-item">(remove)</span>
-    `
-    document.getElementById('ul_list').appendChild(listItem)
-    // Add event listener to remove item
-    listItem.querySelector('.remove-item').addEventListener('click',removeItem)
-    // Add event listener to complete item
-    listItem.querySelector('.todo-item').addEventListener('click', completeItem)
-    // Clear input box
-    document.getElementById('input_box').value = "";
+    const url = 'http://localhost:8080/api/todoitems/';
+    const requestOptions = {
+      method: 'POST',
+      body: JSON.stringify({
+       title: document.getElementById('input_box').value
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      }
+    }
+    async function postData() {
+      try {
+        const response = await fetch(url, requestOptions);
+        if(response.ok) {
+          const jsonResponse = await response.json();
+          addToDoItems(jsonResponse);
+        } else {
+          throw new Error('Request failed!');
+        }
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    postData();
+    // Empty the input box after creating todo item
+    document.getElementById('input_box').value = ""
   } else {
     alert('Please enter a value!')
   }
 }
 
+// add todo items in DOM
 function addToDoItems(todoItem) {
   const listItem = document.createElement('li');
   listItem.className = 'list-item';
