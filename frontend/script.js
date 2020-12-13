5e4b68f0d09bc6ece1f9da8f92edc37e3bfac12b
Cemil Okay <cemilokay@gmail.com>
2020-12-13 20:43:02 +0000

    Save item on keypress Enter

--- frontend/script.js
+++ frontend/script.js
@@ -1,31 +1,36 @@
 // As a user, I can enter the a task name in the input and CLICK button to add into the list.
+// As a user, I can hit ENTER to add a todo item.
 
 document.getElementById('add_item').addEventListener('click', saveItemToDatabase);
+document.getElementById('input_box').addEventListener('keypress', saveItemToDatabase);
 
-async function saveItemToDatabase() {
-  const url = 'http://127.0.0.1:8080/api/todoitems/';
 
-  const requestOptions = {
-    method: 'POST',
-    body: JSON.stringify({
-      title: document.getElementById('input_box').value
-    }),
-    headers: {
-      "Content-Type": "application/json",
+async function saveItemToDatabase(e) {
+  if(e.key === 'Enter' || e.type === 'click') {
+    const url = 'http://127.0.0.1:8080/api/todoitems/';
+
+    const requestOptions = {
+      method: 'POST',
+      body: JSON.stringify({
+        title: document.getElementById('input_box').value
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      }
     }
-  }
 
-  try {
-    const response = await fetch(url, requestOptions);
-    if(response.ok) {
-      const jsonResponse = await response.json();
-      // Write what do you want to do with the response
-      addItemToDOM(jsonResponse);
-    } else {
-      throw new Error('Request failed!');
+    try {
+      const response = await fetch(url, requestOptions);
+      if(response.ok) {
+        const jsonResponse = await response.json();
+        // Write what do you want to do with the response
+        addItemToDOM(jsonResponse);
+      } else {
+        throw new Error('Request failed!');
+      }
+    } catch (error) {
+      console.log(error)
     }
-  } catch (error) {
-    console.log(error)
   }
 }
 
