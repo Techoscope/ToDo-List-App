fa58febf102c7b3ed9a8e371db20a5d2fbf1cba5
Cemil Okay <cemilokay@gmail.com>
2020-12-13 19:53:52 +0000

    Save item to the database and display in the DOM

--- frontend/script.js
+++ frontend/script.js
@@ -1,8 +1,8 @@
-// As a user, I can enter the a task name in the input and click button to add into the list.
+// As a user, I can enter the a task name in the input and CLICK button to add into the list.
 
-document.getElementById('add_item').addEventListener('click', addItem);
+document.getElementById('add_item').addEventListener('click', saveItemToDatabase);
 
-async function addItem() {
+async function saveItemToDatabase() {
   const url = 'http://127.0.0.1:8080/api/todoitems/';
 
   const requestOptions = {
@@ -20,10 +20,22 @@ async function addItem() {
     if(response.ok) {
       const jsonResponse = await response.json();
       // Write what do you want to do with the response
-      console.log(jsonResponse)
+      addItemToDOM(jsonResponse);
+    } else {
+      throw new Error('Request failed!');
     }
-    throw new Error('Request failed!');
   } catch (error) {
     console.log(error)
   }
+}
+
+function addItemToDOM(todoObject) {
+  const listItem = document.createElement('li');
+  listItem.className = 'list-item';
+  listItem.innerHTML = `
+    <span class="todo-item">${todoObject.title}</span>
+    <span class="edit-item">(edit)</span>
+    <span class="remove-item">(remove)</span>
+  `
+  document.getElementById('ul_list').appendChild(listItem);
 }
