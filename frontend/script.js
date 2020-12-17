5f116b2d2b999dc8942cc0e0655167925271ff75
Cemil Okay <cemilokay@gmail.com>
2020-12-17 03:54:54 +0000

    Fix saveItem function

--- frontend/script.js
+++ frontend/script.js
@@ -14,33 +14,35 @@ document.getElementById('input_box').addEventListener('keypress', saveItemToData
 
 
 async function saveItemToDatabase(e) {
-  if((e.key === 'Enter' || e.type === 'click') && document.getElementById('input_box').value.trim()) {
-    const url = 'http://127.0.0.1:8080/api/todoitems/';
+  if(e.key === 'Enter' || e.type === 'click') {
+    if(document.getElementById('input_box').value.trim()) {
+      const url = 'http://127.0.0.1:8080/api/todoitems/';
 
-    const requestOptions = {
-      method: 'POST',
-      body: JSON.stringify({
-        title: document.getElementById('input_box').value
-      }),
-      headers: {
-        "Content-Type": "application/json",
+      const requestOptions = {
+        method: 'POST',
+        body: JSON.stringify({
+          title: document.getElementById('input_box').value
+        }),
+        headers: {
+          "Content-Type": "application/json",
+        }
       }
-    }
 
-    try {
-      const response = await fetch(url, requestOptions);
-      if(response.ok) {
-        const jsonResponse = await response.json();
-        // Write what do you want to do with the response
-        addItemToDOM(jsonResponse);
-      } else {
-        throw new Error('Request failed!');
+      try {
+        const response = await fetch(url, requestOptions);
+        if(response.ok) {
+          const jsonResponse = await response.json();
+          // Write what do you want to do with the response
+          addItemToDOM(jsonResponse);
+        } else {
+          throw new Error('Request failed!');
+        }
+      } catch (error) {
+        console.log(error)
       }
-    } catch (error) {
-      console.log(error)
+    } else {
+      alert('Input Box cannot be empty')
     }
-  } else {
-    alert('Input Box cannot be empty')
   }
 }
 
